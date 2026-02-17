dafa99c63eed57a905a97c9f2d2800d6512c4092
kwameessien <180320488+kwameessien@users.noreply.github.com>
2026-02-17 10:47:03 -0500

    Created routes for POST, GET and DELETE

--- routes/todo.js
+++ routes/todo.js
@@ -4,4 +4,8 @@ const { create, read, removeTodo } = require('../controller');
 
 const router = express.Router();
 
+router.post('/todo/create', create);
+router.get('/todos', read);
+router.delete('/todo/:id', removeTodo);
+
 module.exports = router;
