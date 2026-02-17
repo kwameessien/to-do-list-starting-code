7ca89e0d8c28a4d635e843913b038c983eb8613b
kwameessien <180320488+kwameessien@users.noreply.github.com>
2026-02-17 09:46:34 -0500

    set up PostgreSQL

--- database.js
+++ database.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_DATABASE || 'todo',
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 5432,
 });
 
 module.exports = pool;
--- todo.sql
+++ todo.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE todo;
 
+\c todo
+
 CREATE TABLE todo(
     todo_id SERIAL PRIMARY KEY,
-    description VARCHAR(225) NOT NULL 
+    description VARCHAR(255) NOT NULL
 );
