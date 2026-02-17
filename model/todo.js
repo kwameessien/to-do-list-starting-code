193a6f7729717f10e375f5257d69a2f98f7873f4
kwameessien <180320488+kwameessien@users.noreply.github.com>
2026-02-17 10:04:15 -0500

    Created function create that will insert a description in the todo table

--- model/todo.js
+++ model/todo.js
@@ -1,1 +1,8 @@
 const pool = require('./database');
+
+const create = (description) => pool.query(
+  'INSERT INTO todo (description) VALUES ($1) RETURNING *',
+  [description],
+);
+
+module.exports = { create };
